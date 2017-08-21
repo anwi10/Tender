d67c89e43b765e4da9ce6f4acc6a5b08500ee25e
Anders Winter <awint15@student.sdu.dk>
2017-08-21 14:57:00 -0700

    First commit

--- app/src/main/java/com/example/anderswinter/tender/MainActivity.java
+++ app/src/main/java/com/example/anderswinter/tender/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        .onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
